96b7eeb1c92faf7b7529f625c99160cce370321c
Souleymane Ba <sbjule2180@gmail.com>
2026-07-14 10:12:09 +0000

    ajout des monitoring dans mon front

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import './index.css';
+import { createApp } from "vue";
+import { createRouter } from "vue-router";
+import App from "./App.vue";
+import router from "./router";
 
-// Point d'entrée de l'application Vue :
-// on crée l'app, on branche le routeur, puis on l'attache à #app dans index.html.
-createApp(App).use(router).mount('#app');
+
+import * as Sentry from "@sentry/vue";
+
+import LogRocket from 'logrocket'
+LogRocket.init('pat:r0pvox:eco-stock:zr7fLuqJRro693_EeXMkjrkof5JPn5cC_gFPkhqjMzI')
+const app = createApp({App});
+
+Sentry.init({
+  app,
+  dsn: "https://4e483ee6c6c154ebb61eca107c86708a@o4511713491025920.ingest.de.sentry.io/4511733049655376",
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/vue/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: []
+  }
+});
+
+app.use(router);
+app.mount("#app");
